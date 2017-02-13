c8eeb41a0dca472a726456eada23554ff451bad5
Daniel Nixon <dan.nixon@gmail.com>
2017-02-13 13:19:00 +1100

    Deprecations.

--- core/src/main/scala/org/danielnixon/extrawarts/DateFormatPartial.scala
+++ core/src/main/scala/org/danielnixon/extrawarts/DateFormatPartial.scala
@@ -1,5 +1,6 @@
 package org.danielnixon.extrawarts
 
+@deprecated("Use the Java 8 Time API instead.", "0.3.0")
 object DateFormatPartial extends ClassMethodWart(
   "java.text.DateFormat",
   "parse",
--- sbt-plugin/src/main/scala/org/danielnixon/extrawarts/ExtraWart.scala
+++ sbt-plugin/src/main/scala/org/danielnixon/extrawarts/ExtraWart.scala
@@ -3,12 +3,14 @@ package org.danielnixon.extrawarts
 import wartremover.Wart
 
 object ExtraWart {
+  @deprecated("Use the Java 8 Time API instead.", "0.3.0")
   val DateFormatPartial: Wart = wart("DateFormatPartial")
   val EnumerationPartial: Wart = wart("EnumerationPartial")
   val FutureObject: Wart = wart("FutureObject")
   val GenMapLikePartial: Wart = wart("GenMapLikePartial")
   val GenTraversableLikeOps: Wart = wart("GenTraversableLikeOps")
   val GenTraversableOnceOps: Wart = wart("GenTraversableOnceOps")
+  @deprecated("Use wartremover-contrib's OldTime wart instead.", "0.3.0")
   val LegacyDateTimeCode: Wart = wart("LegacyDateTimeCode")
   val ScalaGlobalExecutionContext: Wart = wart("ScalaGlobalExecutionContext")
   val StringOpsPartial: Wart = wart("StringOpsPartial")
