1a599e6c5072bc1423e1683488eab9deee5a19b0
Daniel Nixon <dan.nixon@gmail.com>
2017-10-11 11:07:19 +1100

    Probably want these.

--- sbt-plugin/src/main/scala/org/danielnixon/extrawarts/ExtraWart.scala
+++ sbt-plugin/src/main/scala/org/danielnixon/extrawarts/ExtraWart.scala
@@ -10,7 +10,9 @@ object ExtraWart {
   val GenTraversableOnceOps: Wart = wart("GenTraversableOnceOps")
   val ScalaGlobalExecutionContext: Wart = wart("ScalaGlobalExecutionContext")
   val StringOpsPartial: Wart = wart("StringOpsPartial")
+  val ThrowablePartial: Wart = wart("ThrowablePartial")
   val TraversableOnceOps: Wart = wart("TraversableOnceOps")
+  val UnsafeContains: Wart = wart("UnsafeContains")
 
   private def wart(name: String) = {
     Wart.custom(s"org.danielnixon.extrawarts.$name")
